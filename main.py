0ec2db778853ac2b83308cbbe731edda137704dd
Bunatyan <bunpog7671@yandex.ru>
2023-04-05 00:55:02 +0300

    bots birthday

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello world!")
+import disnake
+from disnake.ext import commands
+from yt_dlp import YoutubeDL
+import config
+
+bot = commands.Bot(command_prefix = '!', intents = disnake.Intents.all())
+
+@bot.event
+async def on_ready():
+    print("Hello world!")
+
+@bot.event
+async def on_message(message):
+    if message.author.id != bot.user.id:
+        await message.reply("sam takoi")
+
+bot.run(config.token)
