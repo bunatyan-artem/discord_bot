a4e21e1a892c87bd60df79975caab5de5daf4d03
Bunatyan <bunpog7671@yandex.ru>
2023-04-08 17:08:20 +0300

    add queue

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import disnake
+import asyncio
 from disnake.ext import commands
 from yt_dlp import YoutubeDL
 
@@ -9,26 +10,39 @@ YTDL_OPTIONS = {'format': 'bestaudio/best', 'noplaylist': "True", 'simulate': 'T
 FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
 
 vcs = {}
+queue = {}
 
 @bot.event
 async def on_ready():
-    print("Hello world!")
+    print("bot is on")
 
 @bot.command()
 async def play(ctx, url: str):
-    print(url)
+    vc = ctx.guild.voice_client
     try:
-        vc = await ctx.author.voice.channel.connect()
+        if not vc:
+            vc = await ctx.author.voice.channel.connect()
         vcs[vc.guild.id] = vc
         with YoutubeDL(YTDL_OPTIONS) as ytdl:
-            info = ytdl.extract_info(url, download=False)
-        link = info.get("url", None)
-        title = info['title']
-
-        vcs[ctx.guild.id].play(disnake.FFmpegPCMAudio(
-            source=link, **FFMPEG_OPTIONS))
+            info = ytdl.extract_info(url, download = False)
+        if ctx.guild.id not in queue:
+            queue[ctx.guild.id] = []
+        queue[ctx.guild.id].append(info)
+        if not vc.is_playing():
+            while True:
+                if len(queue[ctx.guild.id]) == 0:
+                    await vcs[ctx.guild.id].disconnect()
+                    break
+                link = queue[ctx.guild.id][0].get("url", None)
+                vcs[ctx.guild.id].play(disnake.FFmpegPCMAudio(source = link, **FFMPEG_OPTIONS))
+                while vc.is_playing():
+                    await asyncio.sleep(1)
+                if len(queue[ctx.guild.id]) > 0:
+                    queue[ctx.guild.id].pop(0)
+                else:
+                    break
 
     except:
-        await ctx.send("подключись к каналу, баран")
+        await ctx.send("ошибка")
 
 bot.run(config.token)
