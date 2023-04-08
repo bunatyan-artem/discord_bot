c085748eeafdd89f928b6588d3ba79477dd6a88c
Bunatyan <bunpog7671@yandex.ru>
2023-04-08 19:25:23 +0300

    add pause, resume, stop and repeat

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ from yt_dlp import YoutubeDL
 
 import config
 
-bot = commands.Bot(command_prefix = '!', intents = disnake.Intents.all())
+bot = commands.Bot(command_prefix = '!!', intents = disnake.Intents.all())
 YTDL_OPTIONS = {'format': 'bestaudio/best', 'noplaylist': "True", 'simulate': 'True', 'key': 'FFmpegExtractAudio'}
 FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
 
 vcs = {}
 queue = {}
+repeat_flag = {}
 
 @bot.event
 async def on_ready():
@@ -22,6 +23,8 @@ async def play(ctx, url: str):
     try:
         if not vc:
             vc = await ctx.author.voice.channel.connect()
+            queue[ctx.guild.id] = []
+            repeat_flag[ctx.guild.id] = False
         vcs[vc.guild.id] = vc
         with YoutubeDL(YTDL_OPTIONS) as ytdl:
             info = ytdl.extract_info(url, download = False)
@@ -35,14 +38,43 @@ async def play(ctx, url: str):
                     break
                 link = queue[ctx.guild.id][0].get("url", None)
                 vcs[ctx.guild.id].play(disnake.FFmpegPCMAudio(source = link, **FFMPEG_OPTIONS))
-                while vc.is_playing():
+                while vc.is_playing() or vc.is_paused():
                     await asyncio.sleep(1)
-                if len(queue[ctx.guild.id]) > 0:
+                if len(queue[ctx.guild.id]) > 0 and not repeat_flag[ctx.guild.id]:
                     queue[ctx.guild.id].pop(0)
-                else:
+                elif not repeat_flag[ctx.guild.id]:
                     break
 
     except:
+        await ctx.send("ошибка (play)")
+
+@bot.command()
+async def pause(ctx):
+    try:
+        vcs[ctx.guild.id].pause()
+    except:
         await ctx.send("ошибка")
+
+@bot.command()
+async def stop(ctx):
+    try:
+        vcs[ctx.guild.id].stop()
+        await vcs[ctx.guild.id].disconnect()
+    except:
+        await ctx.send("ошибка")
+
+@bot.command()
+async def resume(ctx):
+    try:
+        vcs[ctx.guild.id].resume()
+    except:
+        await ctx.send("ошибка")
+
+@bot.command()
+async def repeat(ctx):
+    if repeat_flag[ctx.guild.id]:
+        repeat_flag[ctx.guild.id] = False
+    else:
+        repeat_flag[ctx.guild.id] = True
 
 bot.run(config.token)
