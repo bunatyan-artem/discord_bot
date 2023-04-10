0612cbcea3b563da979f06cae36db8f34dd15d01
Bunatyan <bunpog7671@yandex.ru>
2023-04-10 22:08:21 +0300

    slash_commands

--- main.py
+++ main.py
@@ -18,105 +18,118 @@ repeat_flag = {}
 async def on_ready():
     print("bot is on")
 
-@bot.command()
-async def play(ctx, request: str):
+@bot.slash_command()
+async def play(inter, request: str):
     """order a song (use URL from YT or just type the song's name)"""
-    vc = ctx.guild.voice_client
+    vc = inter.guild.voice_client
     try:
         with YoutubeDL(YTDL_OPTIONS) as ytdl:
             if 'https://' in request:
                 info = ytdl.extract_info(request, download = False)
+                await inter.send(info['duration_string'])
             else:
                 try:
                     info = ytdl.extract_info(f"ytsearch:{request}", download=False)['entries'][0]
-                    await ctx.send(f"{info['webpage_url']}\n{info['duration_string']}")
+                    await inter.send(f"{info['webpage_url']}\n{info['duration_string']}")
                 except:
-                    await ctx.send(f'не могу найти в ютубе "{request}"')
+                    await inter.send(f'не могу найти в ютубе "{request}"')
                     return
         if not vc:
-            vc = await ctx.author.voice.channel.connect()
-            queue_list[ctx.guild.id] = []
-            repeat_flag[ctx.guild.id] = False
+            vc = await inter.author.voice.channel.connect()
+            queue_list[inter.guild.id] = []
+            repeat_flag[inter.guild.id] = False
             song = 0
         vcs[vc.guild.id] = vc
-        queue_list[ctx.guild.id].append(info)
+        queue_list[inter.guild.id].append(info)
         if not vc.is_playing():
             while True:
-                if len(queue_list[ctx.guild.id]) == 0:
-                    await vcs[ctx.guild.id].disconnect()
+                if len(queue_list[inter.guild.id]) == 0:
+                    await vcs[inter.guild.id].disconnect()
                     break
-                link = queue_list[ctx.guild.id][song].get("url", None)
+                link = queue_list[inter.guild.id][song].get("url", None)
                 if repeat_flag:
                     song += 1
-                    if song == len(queue_list[ctx.guild.id]):
+                    if song == len(queue_list[inter.guild.id]):
                         song = 0
-                vcs[ctx.guild.id].play(disnake.FFmpegPCMAudio(source = link, **FFMPEG_OPTIONS))
+                vcs[inter.guild.id].play(disnake.FFmpegPCMAudio(source = link, **FFMPEG_OPTIONS))
                 while vc.is_playing() or vc.is_paused():
                     await asyncio.sleep(2)
-                if len(queue_list[ctx.guild.id]) > 0 and not repeat_flag[ctx.guild.id]:
-                    queue_list[ctx.guild.id].pop(0)
-                elif not repeat_flag[ctx.guild.id] or not vc.is_connected():
+                if len(queue_list[inter.guild.id]) > 0 and not repeat_flag[inter.guild.id]:
+                    queue_list[inter.guild.id].pop(0)
+                elif not repeat_flag[inter.guild.id] or not vc.is_connected():
                     break
 
     except:
-        await ctx.send("ошибка (play)")
+        await inter.send("ошибка (play)")
 
-@bot.command()
-async def pause(ctx):
+@bot.slash_command()
+async def pause(inter):
     """pause/resume playback"""
     try:
-        if vcs[ctx.guild.id].is_paused():
-            vcs[ctx.guild.id].resume()
+        if vcs[inter.guild.id].is_paused():
+            vcs[inter.guild.id].resume()
+            await inter.send("player resumed")
         else:
-            vcs[ctx.guild.id].pause()
+            vcs[inter.guild.id].pause()
+            await inter.send("player paused")
     except:
-        await ctx.send("ошибка")
+        await inter.send("ошибка")
 
-@bot.command()
-async def skip(ctx):
+@bot.slash_command()
+async def skip(inter):
     """skip current track"""
     try:
-        vcs[ctx.guild.id].stop()
+        vcs[inter.guild.id].stop()
+        await inter.send("song skipped")
     except:
-        await ctx.send("ошибка")
+        await inter.send("ошибка")
 
-@bot.command()
-async def wrong(ctx):
+@bot.slash_command()
+async def wrong(inter):
     """remove last added track"""
     try:
-        queue_list[ctx.guild.id].pop()
+        await inter.send(f"{queue_list[inter.guild.id][-1]['title']} was removed from queue")
+        queue_list[inter.guild.id].pop()
     except:
-        await ctx.send("ошибка")
+        await inter.send("ошибка")
 
-@bot.command()
-async def stop(ctx):
+@bot.slash_command()
+async def stop(inter):
     """stop playback"""
     try:
-        vcs[ctx.guild.id].stop()
-        await vcs[ctx.guild.id].disconnect()
+        vcs[inter.guild.id].stop()
+        await vcs[inter.guild.id].disconnect()
+        await inter.send("player stopped")
     except:
-        await ctx.send("ошибка")
+        await inter.send("ошибка")
 
-@bot.command()
-async def repeat(ctx):
-    """toogle repeat mode for current queue"""
+@bot.slash_command()
+async def repeat(inter):
+    """toggle repeat mode for current queue"""
     try:
-        if repeat_flag[ctx.guild.id]:
-            repeat_flag[ctx.guild.id] = False
+        if repeat_flag[inter.guild.id]:
+            repeat_flag[inter.guild.id] = False
         else:
-            repeat_flag[ctx.guild.id] = True
+            repeat_flag[inter.guild.id] = True
+        await inter.send("Done")
+        await asyncio.sleep(2)
+        await inter.delete_original_response()
     except:
-        await ctx.send("ошибка")
+        await inter.send("ошибка")
 
-@bot.command()
-async def queue(ctx):
+@bot.slash_command()
+async def queue(inter):
     """print current queue"""
     try:
         queue_text = "Queue:\n"
-        for i in range(len(queue_list[ctx.guild.id])):
-            queue_text += f"{i + 1}) {queue_list[ctx.guild.id][i]['title']}\n"
-        await ctx.send(queue_text)
+        for i in range(len(queue_list[inter.guild.id])):
+            queue_text += f"{i + 1}) {queue_list[inter.guild.id][i]['title']}\n"
+        await inter.send(queue_text)
     except:
-        await ctx.send("ошибка")
+        await inter.send("ошибка")
 
 bot.run(config.token)
+
+@bot.command()
+async def help(ctx):
+    await ctx.send("There is no help..")
